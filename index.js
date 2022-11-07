1fa9bdf1110681f22f204fa7a57389904aec1f78
hienthuy <hiendo9910@gmail.com>
2022-11-07 07:46:02 +0700

    B2

--- index.js
+++ index.js
@@ -14,9 +14,34 @@ function totalSalary() {
     var totalSalary = 0;
     content = "";
     totalSalary = salaryPerDay * workDays;
-    content = "Tổng tiền lương nhân viên là: " + totalSalary;
+    content = "=> Tổng tiền lương nhân viên là: " + totalSalary;
     document.getElementById("print").innerHTML = content;
     document.getElementById("print").classList.add("info");
     
 }
 
+ 
+ //Bài 2: Tính giá trị trung bình
+ /**SƠ ĐỒ 3 KHỐI
+  * input: nhập vào 5 số thực
+  * process: 
+  *      1. Input: nhâp vào 5 số thực: numR1 = 15.5, numR2 = -3.5,
+  *       numR3 = 30, numR4 = 100, numR5 = 0
+  *      2. Tính giá trị trung bình = (numR1 + numR2 + numR3 + numR4 + numR5) / 5
+  *      3. In kết quả
+  * output: Giá trị trung bình
+  * 
+  */
+ function averageValue() {
+    var numR1 = document.getElementById("numR1").value * 1;
+    var numR2 = document.getElementById("numR2").value * 1;
+    var numR3 = document.getElementById("numR3").value * 1;
+    var numR4 = document.getElementById("numR4").value * 1;
+    var numR5 = document.getElementById("numR5").value * 1;
+    var averageValue = 0;
+    var content = "";
+    var averageValue = (numR1 + numR2 + numR3 + numR4 + numR5)/5;
+    content = "=> Giá trị trung bình của 5 số thực là: " + averageValue;
+    document.getElementById("show").innerHTML = content;
+    document.getElementById("show").classList.add("info");
+ }
